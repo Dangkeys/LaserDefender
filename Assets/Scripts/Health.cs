c22ecc387e50acc16f0118f7ef714030ea4a8b28
Dangkeys <TajdangZaa@gmail.com>
2023-09-23 18:32:28 +0700

    Adding Parallax background

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,17 +6,40 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     [SerializeField] int health = 50;
+    [SerializeField] ParticleSystem hitEffect;
+    [SerializeField] bool applyCameraShake;
+    CameraShake cameraShake;
+    private void Awake() {
+        cameraShake = Camera.main.GetComponent<CameraShake>();
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         DamageDealer damageDealer = other.GetComponent<DamageDealer>();
         if (damageDealer == null) return;
         TakeDamage(damageDealer.GetDamage());
+        PlayHitEffect();
+        ShakeCamera();
         damageDealer.Hit();
     }
+
+    private void ShakeCamera()
+    {
+        if(applyCameraShake && cameraShake != null)
+            cameraShake.Play();
+    }
+
     void TakeDamage(int damage)
     {
         health -= damage;
         if(health <= 0)
             Destroy(gameObject);
+    }
+    void PlayHitEffect()
+    {
+        if(hitEffect != null)
+        {
+            ParticleSystem instance = Instantiate(hitEffect, transform.position, Quaternion.identity);
+            Destroy(instance.gameObject, instance.main.duration + instance.main.startLifetime.constantMax);
+        }
     }
 }
