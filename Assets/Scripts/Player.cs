38a41a3e5f263a27828b7e7909429533341e3473
Dangkeys <TajdangZaa@gmail.com>
2023-09-23 16:18:34 +0700

    Added shooting mechanic

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,44 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
+
 public class Player : MonoBehaviour
 {
-    [SerializeField] float moveSpeed;
-    Vector2 minBounds;
-    Vector2 maxBounds;
-    Vector2 moveInput;
+    [SerializeField] float moveSpeed = 5f;
+    Vector2 rawInput;
 
     [SerializeField] float paddingLeft;
     [SerializeField] float paddingRight;
     [SerializeField] float paddingTop;
     [SerializeField] float paddingBottom;
+    
+    Vector2 minBounds;
+    Vector2 maxBounds;
+
+    Shooter shooter;
+
+    void Awake()
+    {
+        shooter = GetComponent<Shooter>();
+    }
+
     void Start()
     {
         InitBounds();
     }
+
     void Update()
     {
         Move();
     }
+
     void InitBounds()
     {
         Camera mainCamera = Camera.main;
-        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0, 0));
-        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
+        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0,0));
+        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1,1));
     }
-    private void Move()
+
+    void Move()
     {
-        Vector2 delta = moveInput * Time.deltaTime * moveSpeed;
+        Vector2 delta = rawInput * moveSpeed * Time.deltaTime;
         Vector2 newPos = new Vector2();
         newPos.x = Mathf.Clamp(transform.position.x + delta.x, minBounds.x + paddingLeft, maxBounds.x - paddingRight);
         newPos.y = Mathf.Clamp(transform.position.y + delta.y, minBounds.y + paddingBottom, maxBounds.y - paddingTop);
-        transform.position  = newPos;
+        transform.position = newPos;
     }
 
-    void OnMove(InputValue inputValue)
+    void OnMove(InputValue value)
     {
-        moveInput = inputValue.Get<Vector2>();
+        rawInput = value.Get<Vector2>();
+    }
+
+    void OnFire(InputValue value)
+    {
+        if(shooter != null)
+        {
+            shooter.isFiring = value.isPressed;
+        }
     }
 }
